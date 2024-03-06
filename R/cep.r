6a51496b4e73aa8e1d5a910e731895d2b6e5dd46
nsj3 <stephen.juggins@gmail.com>
2024-03-06 11:27:35 +0000

    0.1-3 update

--- R/cep.r
+++ R/cep.r
@@ -77,8 +77,12 @@ read.Tilia <- function(fName, removeChron=FALSE) {
     stop("Expecting filename as first argument")
   ret <- .Call("ReadTiliaFile", fName, PACKAGE="riojaExtra")
   if (!is.null(ret[[1]])) {
-    df <- as.data.frame(ret[[1]])
-    rownames(df) <- make.unique(ret[[2]])
+    nms <- names(ret[[1]])
+    names(ret[[1]]) <- iconv(nms, "ISO_8859-2", "UTF-8")
+    df <- as.data.frame(ret[[1]], check.names = TRUE)
+#    rownames(df) <- make.unique(ret[[2]])
+    
+    ret[[3]] <- make.unique(iconv(ret[[3]], "ISO_8859-2", "UTF-8"))
     vars=data.frame(CodeNames=ret[[3]], CodeNums=ret[[4]], FullNames=ret[[5]], Sums=ret[[6]])
     levels = data.frame(Names=make.unique(ret[[2]]), Depths = ret[[7]])
     mt <- na.omit(grep(toupper("Chron*"), toupper(colnames(df))))
